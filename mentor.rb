830f3d6fd4df5d0141c324278a0d86e2ac6665e7
lei <mia.wang90@gmail.com>
2022-04-16 19:51:40 +0900

    注釈を追加した

--- mentor.rb
+++ mentor.rb
@@ -5,7 +5,7 @@ class Mentor
         self.name = name
     end
 
-    def self.job
+    def self.job  #クラスメソッドを定義する
         puts "#{self.name}です。私は現役のITプロフェッショナルです。"
     end
 end
@@ -16,7 +16,7 @@ class RailsMentor < Mentor
         self.name = name
     end
 
-    def self.job
+    def self.job #クラスメソッドを定義する
         puts "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります"
     end
 end
@@ -26,3 +26,38 @@ end
 
 Mentor.job
 RailsMentor.job
+
+
+=begin
+class Mentor
+    attr_accessor :name
+
+    def initialize(name)
+        self.name = name
+    end
+
+    def job  #インスタンスメソッドになっている
+        puts "#{self.name}です。私は現役のITプロフェッショナルです。"
+    end
+end
+
+class RailsMentor < Mentor
+    
+    def initialize(name)
+        self.name = name
+    end
+
+    def job #インスタンスメソッドになっている
+        puts "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります"
+    end
+end
+
+    akaide = RailsMentor.new("赤出")
+    kirameki = Mentor.new("煌木")
+　
+Mentor.job　#クラスメソッド未定義のため、unexpected methodというエラーが出るわけ
+RailsMentor.job #クラスメソッド未定義のため、unexpected methodというエラーが出るわけ
+
+akaide.job #インスタンスメソッドを呼び出すことでいけ
+kirameki.job
+=end
